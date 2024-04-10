f275b7230b468aba4fff86f0c13ac7b00306f571
HsinYi <11050034@me.mcu.edu.tw>
2024-04-10 18:28:55 +0800

    feat(ts): 利用物件記錄產品資料

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-let productId: number = 1;
-const productName: string = "圖像 Angular 開發入門 第二版";
-let isShow: boolean = true;
+// let obj = { propA: value };
 
-console.log("Product Id = ", productId);
-console.log("Product Name = ", productName);
-console.log("Is Show = ", isShow);
-
-let creatDate = new Date(); //當下日期
-console.log("Creat Date", creatDate);
-
-let modifyDate: Date | undefined = undefined;
-console.log("ModifyDate Date", modifyDate);
+const product = {
+  id: 1,
+  name: "圖像 Angular 開發入門 第二版",
+  company: "博碩文化",
+  price: 500,
+  isShow: true,
+  creatDate: new Date(2024, 3, 10),
+  modifyDate: undefined,
+};
+console.log(product);
+console.table(product);
