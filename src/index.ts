05f46137abf9226e36e80e2764ae10bc98a41f26
HsinYi <11050034@me.mcu.edu.tw>
2024-04-10 18:48:30 +0800

    feat(ts): 利用陣列記錄多筆產品

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 // let obj = { propA: value };
+// const arr = [1, 2, 3, 4];
 
 import { Product } from "./product";
 
-const product = new Product(
-  1,
-  "圖像 Angular 開發入門 第二版",
-  "博碩文化",
-  500,
-  true,
-  new Date(2024, 3, 10),
-  undefined
-);
-console.log(product);
-console.table(product);
+const products = [
+  new Product(
+    1,
+    "圖像 Angular 開發入門 第二版",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 10),
+    undefined
+  ),
+  new Product(
+    2,
+    "金魚都能懂的 CSS 必學屬性",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024, 3, 10),
+    undefined
+  ),
+];
+console.log(products);
+console.table(products);
