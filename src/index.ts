9253c1f95a50dd31abcdcb11742a8efd8a937525
HsinYi <11050034@me.mcu.edu.tw>
2024-04-10 17:39:41 +0800

    feat(ts): 利用 undefined 變數宣告產品建檔日期

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,6 @@ let isShow: boolean = true;
 console.log("Product Id = ", productId);
 console.log("Product Name = ", productName);
 console.log("Is Show = ", isShow);
+
+let creatDate = undefined;
+console.log("Creat Date", creatDate);
